e1e65f6361df56e9d864dc7dbb1cbffbf3b85896
akunku767 <mrifqi767@gmail.com>
2022-06-14 18:00:32 +0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('css/Style Isi.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('css/all.css') }}"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        
+        <script src="https://kit.fontawesome.com/8e6e4ad827.js" crossorigin="anonymous"></script>
 
         <title>Laravel</title>
 
