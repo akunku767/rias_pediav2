366495cd6ecf21c925b5a7a48c6999410928f3f1
akunku767 <mrifqi767@gmail.com>
2022-06-15 13:53:59 +0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'List Data Days')
 @section('content')
 
+<div class="wrapper" style="padding-left: 260px">
+  <h1 style="text-align: center;">Dashboard</h1>
+  <p>Silahkan pilih menu berikut</h1>
+</div>
+
 {{-- Sintaks sidebar, beri kode #FFFFFF untuk menu aktif, dan beri kode #000000 untuk menu tidak aktif --}}
 @include('layouts.sidebar')
 <script>
