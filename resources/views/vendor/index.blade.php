c4d6c9d1ff9c037e84f6e6b461f2fd2d08bc83c5
akunku767 <mrifqi767@gmail.com>
2022-06-16 09:30:05 +0700

    Merge branch 'master' of https://github.com/akunku767/rias_pedia

--- resources/views/vendor/index.blade.php
+++ resources/views/vendor/index.blade.php
@@ -12,7 +12,7 @@
   <table style="width:100%; border-collapse:collapse">
     <thead>
       <tr>
-        <th>Idsdf</th>
+        <th>Id</th>
         <th>Name</th>
         <th>Phone</th>
         <th>Role_id</th>
