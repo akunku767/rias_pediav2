9703e9a2a62dd9d8a58a4d371d9be35e6c897fbe
SyayidahAfiva <syayidahafiva@gmail.com>
2022-06-16 10:37:55 +0700

    oke

--- resources/views/Hour/index.blade.php
+++ resources/views/Hour/index.blade.php
@@ -3,33 +3,33 @@
 @section('title', 'Rias Pedia')
 @section('content')
 <div class="wrapper" style="padding-left: 260px">
-  <h1 style="text-align: center;">List Data Users</h1>
+  <h1 style="text-align: center;">List Data Vendors</h1>
 
   <div style="margin-bottom: 20px">
-    <a class="tambah" href="{{ url('users/create' ) }}"><i class="fa-solid fa-plus"></i> Tambah Data</a>
+    <a class="tambah" href="{{ url('hours/create' ) }}"><i class="fa-solid fa-plus"></i> Tambah Data</a>
   </div>
 
   <table style="width:100%; border-collapse:collapse">
     <thead>
       <tr>
         <th>Id</th>
-        <th>Name</th>
-        <th>Email</th>
+        <th>Time</th>
+        <th>Day Id</th>
+        <th>Vendor Id</th>
         {{-- <th>Password</th> --}}
-        <th>Role_id</th>
         <th colspan="2">Aksi</th>
       </tr>
     </thead>
     <tbody>
-      @foreach ($users as $user)
+      @foreach ($hours as $hour)
       <tr style="height: 42px">
-        <td style="width: 100px" >{{ $user->id}}</td>
-        <td style="width: 150px" >{{ $user->name }}</td>
-        <td style="width: 150px" >{{ $user->email }}</td>
+        <td style="width: 100px" >{{ $hour->id}}</td>
+        <td style="width: 150px" >{{ $hour->name }}</td>
+        <td style="width: 150px" >{{ $hour->day_id }}</td>
+        <td style="width: 150px" >{{ $hour->vendor_id }}</td>
         {{-- <td style="width: 150px" >{{ $user->password }}</td> --}}
-        <td style="width: 150px" >{{ $user->role_id }}</td>
-        <td style="width: 100px"><button class="btn-green" style="width: auto"><a href="{{ route('users.edit', $user->id) }}">Edit</a></button></td>
-        <form method="POST" action="{{ url('users', $user->id ) }}">
+        <td style="width: 100px"><button class="btn-green" style="width: auto"><a href="{{ route('hours.edit', $hour->id) }}">Edit</a></button></td>
+        <form method="POST" action="{{ url('hours', $hour->id ) }}">
             @csrf
             @method('DELETE')
             <td style="width: 100px"><button class="btn-red" style="cursor: pointer; width: auto">Hapus</button></td>
@@ -44,9 +44,9 @@
 @include('layouts.sidebar')
 <script>
 //   id = role, user, vendor, service, gallery, review, day, hour
-  ["role", "day", "vendor", "service", "gallery", "review", "hour"].forEach(id => document.getElementById(id).style.color = "#000000")
-  document.getElementById("user").style.color = "#FFFFFF"; //tulis menu aktif sesuai tabel
-  document.getElementById("divUser").style.backgroundColor = "#3F51B5"; //div.. itu kotak biru
+  ["role", "day", "vendor", "service", "gallery", "review", "user"].forEach(id => document.getElementById(id).style.color = "#000000")
+  document.getElementById("hour").style.color = "#FFFFFF"; //tulis menu aktif sesuai tabel
+  document.getElementById("divHour").style.backgroundColor = "#3F51B5"; //div.. itu kotak biru
 </script>
 
 @endsection
