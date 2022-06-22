5e269f8fe664cc3df2eecc43823a1b6ce8bc2899
akunku767 <mrifqi767@gmail.com>
2022-06-22 12:59:23 +0700

    Merge branch 'master' of https://github.com/akunku767/rias_pedia

---
+++ app/Http/Controllers/API/DayController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+// use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Day;
+use Validator;
+use App\Http\Resources\Day as DayResource;
+
+class DayController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $days = Day::all();
+    
+        return $this->sendResponse(DayResource::collection($days), 'Days retrieved successfully.');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $day = Day::create($input);
+   
+        return $this->sendResponse(new DayResource($day), 'Day created successfully.');
+    } 
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $day = Day::find($id);
+  
+        if (is_null($day)) {
+            return $this->sendError('Day not found.');
+        }
+   
+        return $this->sendResponse(new DayResource($day), 'Day retrieved successfully.');
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Day $day)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $day->name = $input['name'];
+        $day->save();
+   
+        return $this->sendResponse(new DayResource($day), 'Day updated successfully.');
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Day $day)
+    {
+        $day->delete();
+   
+        return $this->sendResponse([], 'Day deleted successfully.');
+    }
+}
