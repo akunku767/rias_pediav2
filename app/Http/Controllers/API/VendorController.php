93c973140d77a827fae9416c4a572f0a3581b7ec
siskadyah <siskapramesti26@gmail.com>
2022-06-23 13:35:49 +0700

    uy

--- app/Http/Controllers/API/VendorController.php
+++ app/Http/Controllers/API/VendorController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Vendor;
-use Validation;
+use Validator;
 use App\Http\Resources\Vendor as VendorResource;
 
 class VendorController extends BaseController
