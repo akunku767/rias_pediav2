59de59279ab82aeb86f49b49e98d4e79d589abd2
akunku767 <mrifqi767@gmail.com>
2022-06-23 11:04:06 +0700

    am

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\photo;
+use App\Models\Photo;
 use Illuminate\Http\Request;
 
 class PhotoController extends Controller
