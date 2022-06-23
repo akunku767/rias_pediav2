49a68dd665b683c695b2c856365c11e91bd14f5d
akunku767 <mrifqi767@gmail.com>
2022-06-23 15:37:48 +0700

    am

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 {{-- //views/layouts/index.blade.php --}}
 @extends('layouts.app')
-@section('title', 'List Data Days')
+@section('title', 'Rias Pedia')
 @section('content')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
