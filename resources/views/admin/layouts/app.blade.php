39680c7746ac111b99c024622e2fe719444e599b
akunku767 <mrifqi767@gmail.com>
2022-07-21 00:38:56 +0700

    f

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -27,30 +28,50 @@
     <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
     <!-- Your custom styles -->
     <link href="css/custom.css" rel="stylesheet">
-    
+
     <style>
-      ::-webkit-scrollbar, ::-webkit-scrollbar-corner {
+      body::-webkit-scrollbar, ::-webkit-scrollbar-corner {
         width: 8px;
         height: 8px;
       }
 
-      ::-webkit-scrollbar-track {
+      body::-webkit-scrollbar-track {
         background-color: rgb(252, 252, 253);
         background-color: var(--gray-50);
       }
 
-      ::-webkit-scrollbar-thumb {
+      body::-webkit-scrollbar-thumb {
         background-color: rgb(142, 137, 162);
         background-color: var(--gray-600);
         border-radius: 4px;
       }
 
-      ::-webkit-scrollbar-track:vertical {
+      body::-webkit-scrollbar-track:vertical {
         border-left: solid 1px rgba(0, 0, 0, 0.1);
       }
 
-      ::-webkit-scrollbar-track:horizontal {
+      body::-webkit-scrollbar-track:horizontal {
         border-top: solid 1px rgba(0, 0, 0, 0.1);
+      }
+
+      .navbar-sidenav::-webkit-scrollbar, ::-webkit-scrollbar-corner {
+        display: none!important
+      }
+
+      .navbar-sidenav::-webkit-scrollbar-track {
+        display: none!important
+      }
+
+      .navbar-sidenav::-webkit-scrollbar-thumb {
+        display: none!important
+      }
+
+      .navbar-sidenav::-webkit-scrollbar-track:vertical {
+        display: none!important
+      }
+
+      .navbar-sidenav::-webkit-scrollbar-track:horizontal {
+        display: none!important
       }
     </style>
   </head>
