6b084ddf4e1d31d1364fb831b36cb1ee0231648b
bagusbatra <bagusbatr@gmail.com>
2022-07-21 11:59:36 +0700

    p

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -84,20 +84,20 @@
     @include('admin.layouts.logout')
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <!-- Page level plugin JavaScript-->
-    <script src="vendor/chart.js/Chart.js"></script>
-    <script src="vendor/datatables/jquery.dataTables.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
-    <script src="vendor/jquery.selectbox-0.2.js"></script>
-    <script src="vendor/retina-replace.min.js"></script>
-    <script src="vendor/jquery.magnific-popup.min.js"></script>
+    <script src="{{ asset('vendor/chart.js/Chart.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('vendor/jquery.selectbox-0.2.js') }}"></script>
+    <script src="{{ asset('vendor/retina-replace.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery.magnific-popup.min.js') }}"></script>
     <!-- Custom scripts for all pages-->
-    <script src="js/admin.js"></script>
+    <script src="{{ asset('js/admin.js') }}"></script>
     <!-- Custom scripts for this page-->
-    <script src="js/admin-charts.js"></script>
+    <script src="{{ asset('js/admin-charts.js') }}"></script>
   </body>
 </html>
