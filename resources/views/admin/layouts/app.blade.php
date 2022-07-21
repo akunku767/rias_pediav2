c697d507fe28e5312275b97d5b89819a63a77572
akunku767 <mrifqi767@gmail.com>
2022-07-21 11:27:37 +0700

    d

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="Ansonika">
+    <meta name="google-site-verification" content="-3n39au53fTHd6KIrrldXmN3PREAyq2RIAIa1U7Hhyk" />
     <title>@yield('title')</title>
         
     {{-- <!-- Favicons--> --}}
