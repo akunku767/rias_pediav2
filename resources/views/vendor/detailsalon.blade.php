76f495658770231c536ffa51a1675eddd350e346
siskadyah <siskapramesti26@gmail.com>
2022-09-09 10:30:00 +0700

    modified:   resources/views/vendor/detailsalon.blade.php

--- resources/views/vendor/detailsalon.blade.php
+++ resources/views/vendor/detailsalon.blade.php
@@ -328,13 +328,13 @@
 						<div class="box_detail booking">
 							<div class="price">
 								<span>45$ <small>person</small></span>
-								<div class="score"><span>Good<em>350 Reviews</em></span><strong>7.0</strong></div>
+								<div class="score"><span>Good<em>10 Reviews</em></span><strong>5.0</strong></div>
 							</div>
-							<div class="form-group input-dates">
+							<!-- <div class="form-group input-dates">
 								<input class="form-control" type="text" name="dates" placeholder="When..">
 								<i class="icon_calendar"></i>
-							</div>
-							<div class="panel-dropdown">
+							</div> -->
+							<!-- <div class="panel-dropdown">
 								<a href="#">Guests <span class="qtyTotal">1</span></a>
 								<div class="panel-dropdown-content right">
 									<div class="qtyButtons">
@@ -346,10 +346,10 @@
 										<input type="text" name="qtyInput" value="0">
 									</div>
 								</div>
-							</div>
-							<a href="cart-1.html" class="btn_1 full-width purchase">Purchase</a>
+							</div> -->
+							<a href="cart-1.html" class="btn_1 full-width purchase">Contact With Us</a>
 							<a href="wishlist.html" class="btn_1 full-width outline wishlist"><i class="icon_heart"></i> Add to wishlist</a>
-							<div class="text-center"><small>No money charged in this step</small></div>
+							<!-- <div class="text-center"><small>No money charged in this step</small></div> -->
 						</div>
 						<ul class="share-buttons">
 							<li><a class="fb-share" href="#0"><i class="social_facebook"></i> Share</a></li>
