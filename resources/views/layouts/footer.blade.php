7c86905f9e09e3561d68d2c13eca2d236b98d3cb
bagusbatra <bagusbatr@gmail.com>
2022-09-16 11:07:52 +0700

    y

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
     <div class="container margin_60_35">
-        <div class="row">
+        <div class="row pt-4">
             <div class="col-lg-5 col-md-12 p-r-5">
                 <p><img src="{{ asset('img/logo_sticky.png') }}" width="150" height="36" alt=""></p>
-                <p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
+                <p>RiasPedia is one of the beauty e-commerce in Southeast Asia that provides the best 100% original and recommended make up artist and tools vendors. Complete your beauty needs only at RiasPedia, a safe, fun, and reliable platform for searching via the website so you can enjoy a wide selection of interesting vendors around you.</p>
                 <div class="follow_us">
                     <ul>
                         <li>Follow us</li>
@@ -18,10 +18,7 @@
             <div class="col-lg-3 col-md-6 ml-lg-auto">
                 <h5>Useful links</h5>
                 <ul class="links">
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="login.html">Login</a></li>
-                    <li><a href="register.html">Register</a></li>
-                    <li><a href="blog.html">News &amp; Events</a></li>
+                    <li><a href="{{ route("aboutus") }}">About</a></li>
                     <li><a href="{{ url('/about-us/#location') }}">Contacts</a></li>
                 </ul>
             </div>
@@ -42,7 +39,10 @@
                 </form>
                 </div>
             </div>
+            <div class="col-lg-5 col-md-12 p-r-5">
+            </div>
         </div>
+        
         {{-- <!--/row--> --}}
         <hr>
         <div class="row">
